ff9f65f36e0775dfe03dc034ad712dea52313f39
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-22 15:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (83.61%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    int helper(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-        if(i == nums.size()) {
-            return 0;
-        }
-        if(dp[i][prev + 1] != -1) {
-            return dp[i][prev + 1];
-        }
-        int ntaken = helper(i + 1, prev, nums, dp);
-        int taken = prev == -1 or nums[i] > nums[prev] ? 1 + helper(i + 1, i, nums, dp) : 0;
-        return dp[i][prev + 1] = max(ntaken, taken);
-    }
     int lengthOfLIS(vector<int>& nums) {
         vector<int> temp;
         temp.push_back(nums[0]);
