f91595b5304af1cb792836fa5da22dc335b0b512
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-23 00:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (59.07%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -10,9 +10,6 @@ public:
     void helper(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans)
     {
         ans.push_back(ds);
-        if(ind == nums.size()){
-            return;
-        }
         for(int i = ind; i<nums.size(); i++)
         {
             if(i>ind && nums[i]==nums[i-1]) continue;
