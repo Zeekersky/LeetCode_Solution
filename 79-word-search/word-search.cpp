b8773f1094f7879141a485e6ff1781df8943677c
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-26 14:35:45 +0530

    Time: 195 ms (90.62%) | Memory: 9.7 MB (95.16%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int rows = board.size(), cols = board[0].size();
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[0].size(); ++j) {
                 if (helper(i, j, board, word, 0)) {
                     return true;
                 }
@@ -13,21 +12,21 @@ public:
     }
 
 private:
-    bool helper(int row, int col, vector<vector<char>>& board, const string& word, int index) {
-        if (index == word.size()) {
+    bool helper(int row, int col, vector<vector<char>>& board, const string& word, int ind) {
+        if (ind == word.size()) {
             return true;
         }
-        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[index]) {
+        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[ind]) {
             return false;
         }
 
         char temp = board[row][col];
         board[row][col] = '#';
 
-        bool found = helper(row + 1, col, board, word, index + 1) ||
-                     helper(row - 1, col, board, word, index + 1) ||
-                     helper(row, col + 1, board, word, index + 1) ||
-                     helper(row, col - 1, board, word, index + 1);
+        bool found = helper(row + 1, col, board, word, ind + 1) ||
+                     helper(row - 1, col, board, word, ind + 1) ||
+                     helper(row, col + 1, board, word, ind + 1) ||
+                     helper(row, col - 1, board, word, ind + 1);
 
         board[row][col] = temp;
 
