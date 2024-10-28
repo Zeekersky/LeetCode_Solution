a3f48e7fca6d0fc3b0e2002cd8524da444cd2fd8
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-28 12:44:07 +0530

    Time: 2 ms (36.68%) | Memory: 77.4 MB (74.42%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -16,39 +16,33 @@ public:
         ListNode *result = new ListNode();
         ListNode *temp3 = result;
         int carry = 0;
-        while (temp1 != NULL || temp2 != NULL)
-        {
-            if (temp1 != NULL && temp2 != NULL)
-            {
+        while (temp1 != NULL || temp2 != NULL){
+            if (temp1 != NULL && temp2 != NULL){
                 int sum = temp1->val + temp2->val + carry;
                 carry = sum / 10;
                 temp3->val = sum % 10;
                 temp1 = temp1->next;
                 temp2 = temp2->next;
             }
-            else if (temp1 != NULL && temp2 == NULL)
-            {
+            else if (temp1 != NULL && temp2 == NULL){
                 int sum = temp1->val + carry;
                 carry = sum / 10;
                 temp3->val = sum % 10;
                 temp1 = temp1->next;
             }
-            else if (temp1 == NULL && temp2 != NULL)
-            {
+            else if (temp1 == NULL && temp2 != NULL){
                 int sum = temp2->val + carry;
                 carry = sum / 10;
                 temp3->val = sum % 10;
                 temp2 = temp2->next;
             }
 
-            if (temp1 != NULL || temp2 != NULL)
-            {
+            if (temp1 != NULL || temp2 != NULL){
                 temp3->next = new ListNode();
                 temp3 = temp3->next;
             }
         }
-        if (carry != 0)
-        {
+        if (carry != 0){
             temp3->next = new ListNode();
             temp3 = temp3->next;
             temp3->val = carry;
