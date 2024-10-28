fa8de757339d527c0b654350ae47f516254f9208
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-28 17:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (88.43%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,7 +12,16 @@ public:
         return dp[n] = one + two;
     }
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return helper(n, dp);
+        // vector<int> dp(n + 1, -1);
+        // return helper(n, dp);
+        int prev=1, prev2=1, curri;
+        if(n<=1) return 1;
+        for(int i=2;i<=n;i++)
+        {
+            curri = prev+prev2;
+            prev2 = prev;
+            prev = curri;
+        }
+        return curri;
     }
 };
