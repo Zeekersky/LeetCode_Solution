7735e498db0e4bbe39ff1fe8044baad992b3a3ef
AkashPal0 <pal82087@gmail.com>
2024-10-29 16:13:03 +0530

    Geek Jump Recursive Solution Added

---
+++ GFG-Geek-Jump/RecursiveSol.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int minimumEnergy(vector<int> &height, int n)
+    {
+        // Code here
+        if (n <= 1)
+            return 0;
+        return helper(n - 1, height);
+    }
+    int helper(int ind, vector<int> &height)
+    {
+        if (ind < 0)
+            return 0;
+        if (ind == 1)
+            return abs(height[1] - height[0]);
+        return min(helper(ind - 1, height) + abs(height[ind - 1] - height[ind]),
+                   helper(ind - 2, height) + abs(height[ind - 2] - height[ind]));
+    }
+};
