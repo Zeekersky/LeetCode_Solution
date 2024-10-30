75c5e2905f3199d57366a4c595a9d919ced47435
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-30 12:26:14 +0530

    Time: 11 ms (49.40%) | Memory: 16 MB (49.40%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.length();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && linearSearch(wordDict,s.substr(j, i - j))) {
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+    
+    bool linearSearch(vector<string>& vec, string target) {
+        for (int i = 0; i < vec.size(); i++) {
+            if (vec[i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
