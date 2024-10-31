e8c2ee308ac4c292af7e4fba92d316028fb1546a
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-10-31 10:13:12 +0530

    Time: 11 ms (48.92%) | Memory: 15.9 MB (49.31%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) 
-    {
+    bool wordBreak(string s, vector<string>& wordDict) {
         vector<bool> dp(s.length() + 1, false);
         dp[0] = true;
-        for (int i = 1; i <= s.length(); i++) 
-        {
-            for (int j = 0; j < i; j++) 
-            {
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
                 if (dp[j] && linearSearch(wordDict,s.substr(j, i - j))) 
                 {
                     dp[i]=true;
@@ -17,11 +14,9 @@ public:
         }
         return dp[s.length()];
     }
-    
     bool linearSearch(vector<string>& vec, string target) 
     {
-        for (int i = 0; i < vec.size(); i++) 
-        {
+        for (int i = 0; i < vec.size(); i++) {
             if (vec[i] == target)
                 return true;
         }
