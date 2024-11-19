3164e6ef4337a04d1ff47c0e6b19e54f544abd26
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-11-19 17:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.89%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -23,4 +23,33 @@ public:
         helper(root->right, arr);
         arr.push_back(root->val);
     }
+    void helper_iterative(TreeNode* root, vector<int> &arr)
+    {
+        if(root==NULL) return;
+        stack<TreeNode*> st;
+        stack<TreeNode*> st_print;
+        TreeNode* node = root;
+        while(true)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty()) return;
+                node = st.top();
+                st.pop();
+                st_print.push(node);
+                node = node->right;
+            }
+        }
+        while(!st_print.empty())
+        {
+            root = st_print.top();
+            st_print.pop();
+            arr.push_back(root->val);
+        }
+    }
 };
