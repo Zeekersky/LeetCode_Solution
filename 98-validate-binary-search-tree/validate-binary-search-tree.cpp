18e000eae56162d7ac8b4601dd35668289e89923
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-02 15:57:09 +0530

    Time: 3 ms (7.27%) | Memory: 21.8 MB (7.05%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL) return true;
+        // if((root->left && root->left->val >= root->val) || (root->right && root->right->val <= root->val))
+        //     return false;
+        // return isValidBST(root->left) && isValidBST(root->right);
+        return isValidMargin(root, LONG_MIN, LONG_MAX);
+    }
+    bool isValidMargin(TreeNode* root, long long leftMargin, long long rightMargin)
+    {
+        if(root == NULL) return true;
+        if((root->val <= leftMargin) || (root->val >= rightMargin))
+            return false;
+        return isValidMargin(root->left, leftMargin, root->val) && isValidMargin(root->right, root->val, rightMargin);
+    }
+};
