07c4fb4e58ef5422d929b1ee2392554bba1189db
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-04 04:04:31 +0530

    Time: 3 ms (5.90%) | Memory: 10.6 MB (8.22%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        return mem(nums);
+        // return mem(nums);
+        return tab(nums);
+    }
+    int tab(vector<int> &nums){
+        vector<int> dp(nums.size(), -1);
+        if(nums.empty()) return 0;
+        dp[0] = nums[0];
+        for(int i=1; i<nums.size(); i++){
+            int pick = INT_MIN;
+            if(i>1) pick = dp[i-2] + nums[i];
+            else pick = nums[i];
+            int notPick = dp[i-1];
+            dp[i] = max(pick, notPick);
+        }
+        for(auto it:dp) cout << it << " ";
+        return dp[nums.size()-1];
     }
     int mem(vector<int>& nums) {
         vector<int> dp(nums.size(), -1);
