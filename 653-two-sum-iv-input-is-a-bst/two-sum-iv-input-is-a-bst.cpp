efe43e56a8c512bc133c409b162809b861ed07be
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-03 23:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 37.4 MB (34.57%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,24 +9,63 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator {
+public:
+    // Reverse = true -> before
+    // Reverse = false -> next
+    stack<TreeNode*> st;
+    bool reverse = false;
+    BSTIterator(TreeNode* root, bool isReverse) {
+        TreeNode* node = root;
+        reverse = isReverse;
+        while(node){
+            st.push(node);
+            if(!reverse) node = node->left;
+            else node = node->right;
+        }
+    }
+    
+    int next() {
+        TreeNode* node = st.top(); st.pop();
+        int ans = node->val;
+        if(!reverse){
+            if (node->right) {
+                node = node->right;
+                while (node) {
+                    st.push(node);
+                    node = node->left;
+                }
+            }
+        }
+        else{
+            if (node->left) {
+                node = node->left;
+                while (node) {
+                    st.push(node);
+                    node = node->right;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+};
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        vector<int> arr;
-        inorder(root, arr);
-        int left = 0, right = arr.size()-1;
-        while(left<right){
-            int tot = arr[left] + arr[right];
-            if(tot == k) return true;
-            if(tot < k) left++;
-            else right--;
+        if(!root) return false;
+        BSTIterator left(root, false);
+        BSTIterator right(root, true);
+        int l = left.next();
+        int r = right.next();
+        while(l<r){
+            if(l+r == k) return true;
+            if(l+r < k) l = left.next();
+            else r = right.next();
         }
         return false;
-    }
-    void inorder(TreeNode* root, vector<int> &arr){
-        if(!root) return;
-        if(root->left) inorder(root->left, arr);
-        arr.push_back(root->val);
-        if(root->right) inorder(root->right, arr);
     }
 };
