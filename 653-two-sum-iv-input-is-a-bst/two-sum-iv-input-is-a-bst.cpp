56ecae32f3a2233cf23edc533003e63736b18143
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-03 23:31:03 +0530

    Time: 1 ms (62.41%) | Memory: 37.2 MB (46.16%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -9,34 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator {
-public:
-    stack<TreeNode*> st;
-    BSTIterator(TreeNode* root) {
-        TreeNode* node = root;
-        while(node){
-            st.push(node);
-            node = node->left;
-        }
-    }
-    
-    int next() {
-        TreeNode* node = st.top(); st.pop();
-        int ans = node->val;
-        if (node->right) {
-            node = node->right;
-            while (node) {
-                st.push(node);
-                node = node->left;
-            }
-        }
-        return ans;
-    }
-    
-    bool hasNext() {
-        return !st.empty();
-    }
-};
+
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
