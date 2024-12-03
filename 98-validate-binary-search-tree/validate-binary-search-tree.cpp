bf2b9819ae01236dc806848efee2a0f1627294b5
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-03 10:29:14 +0530

    Time: 3 ms (7.27%) | Memory: 21.9 MB (6.39%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if(root==NULL) return true;
+        // if(root==NULL) return true;
         return isValidMargin(root, LONG_MIN, LONG_MAX);
     }
     bool isValidMargin(TreeNode* root, long long leftMargin, long long rightMargin)
