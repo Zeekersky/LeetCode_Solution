0ec5e062dcb5cb1f6a7cf14826fa07d662a6d0b7
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-05 00:01:07 +0530

    Time: 2 ms (16.57%) | Memory: 9.4 MB (6.36%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -7,7 +7,6 @@ public:
             dp[i][0] = 1;
         for(int i = 1; i<n; i++)    
             dp[0][i] = 1;
-
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 dp[i][j]=dp[i-1][j]+ dp[i][j-1];
