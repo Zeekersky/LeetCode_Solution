06de30dd610dd90e3d74ca3d1ab85c176e3be4cd
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2024-12-31 00:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (12.38%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -12,7 +12,7 @@ public:
         if(n==0)
             return;
         int fact = 1;
-        for (int i = 1; i < n; ++i) {
+        for (int i = 2; i < n; ++i) {
             fact *= i;
         }
         int next = k/fact;
