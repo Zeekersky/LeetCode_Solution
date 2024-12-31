3ddd4bb6acc6fac9bef26b8112aa0c9225d474a5
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-01 01:54:42 +0530

    Time: 32 ms (52.26%) | Memory: 22.1 MB (40.68%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         if(amount == 0) return 0;
-        // vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        // int ans = helper(coins.size()-1, coins, amount, dp);
-        // vector<vector<int>> dp(coins.size(), vector<int>(amount+1, 0));
         vector<int> prev(amount+1, 0);
         vector<int> curr(amount+1, 0);
         for(int j=0; j<=amount; j++){
@@ -24,18 +21,5 @@ public:
         int ans = prev[amount];
         if(ans >= 1e9) return -1;
         else return ans;
-    }
-    int helper(int ind, vector<int> &coins, int amount, vector<vector<int>> &dp){
-        if(ind == 0){
-            if(amount % coins[0] == 0){
-                return amount / coins[0];
-            }
-            return INT_MAX-1;
-        }
-        if (dp[ind][amount] != -1) return dp[ind][amount];
-        int pick = INT_MAX;
-        if(amount >= coins[ind]) pick = 1+helper(ind, coins, amount-coins[ind], dp);
-        int notPick = helper(ind-1, coins, amount, dp);
-        return dp[ind][amount] = min(pick, notPick);
     }
 };
