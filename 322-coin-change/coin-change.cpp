76d13391bd11caa8adec2bade4bcc3c5775b2ed8
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-01 01:54:03 +0530

    Time: 30 ms (55.54%) | Memory: 22.1 MB (40.68%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,22 +4,24 @@ public:
         if(amount == 0) return 0;
         // vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
         // int ans = helper(coins.size()-1, coins, amount, dp);
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, 0));
-        // vector<int> prev
+        // vector<vector<int>> dp(coins.size(), vector<int>(amount+1, 0));
+        vector<int> prev(amount+1, 0);
+        vector<int> curr(amount+1, 0);
         for(int j=0; j<=amount; j++){
             if(j%coins[0] == 0)
-                dp[0][j] = j/coins[0];
-            else dp[0][j] = 1e9;
+                prev[j] = j/coins[0];
+            else prev[j] = 1e9;
         }
         for(int i=1; i<coins.size(); i++){
             for(int j=0; j<=amount; j++){
                 int pick = INT_MAX;
-                if(j >= coins[i]) pick = 1+dp[i][j-coins[i]];
-                int notPick = dp[i-1][j];
-                dp[i][j] = min(pick, notPick);
+                if(j >= coins[i]) pick = 1+curr[j-coins[i]];
+                int notPick = prev[j];
+                curr[j] = min(pick, notPick);
             }
+            prev = curr;
         }
-        int ans = dp[coins.size()-1][amount];
+        int ans = prev[amount];
         if(ans >= 1e9) return -1;
         else return ans;
     }
