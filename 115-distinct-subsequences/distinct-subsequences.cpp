60ecfc8368e49f4bd1cbf5f91cebb93ebdbcd921
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-03 01:17:59 +0530

    Time: 11 ms (91.30%) | Memory: 8.9 MB (90.88%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -3,7 +3,22 @@ public:
     int numDistinct(string s, string t) {
         // vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, -1));
         // return helper(s.size(), t.size(), s, t, dp);
-        return tabulation(s, t);
+        // return tabulation(s, t);
+        return space_optimized(s, t);
+    }
+    int space_optimized(string &s, string &t){
+        int MOD = 1e9 + 7;
+        vector<int> prev(t.size()+1, 0), curr(t.size()+1, 0);
+        for(int i=1; i<=s.size(); i++){
+            prev[0] = 1;
+            for(int j=1; j<=t.size(); j++){
+                if(s[i-1]==t[j-1])
+                    curr[j] = (prev[j-1]+prev[j])%MOD;
+                else curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[t.size()];
     }
     int tabulation(string &s, string &t){
         int MOD = 1e9 + 7;
