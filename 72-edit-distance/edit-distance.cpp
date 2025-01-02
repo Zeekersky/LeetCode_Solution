6b12e3aa844f2e3497355267157d03ad3ca5f8eb
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-03 02:43:45 +0530

    Time: 4 ms (77.84%) | Memory: 13.1 MB (45.42%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
-        return helper(word1.size(), word2.size(), word1, word2, dp);
+        // vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
+        // return helper(word1.size(), word2.size(), word1, word2, dp);
+        return tabulation(word1, word2);
+    }
+    int tabulation(string& s, string& t){
+        vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, 0));
+        for(int i=0; i<=s.size(); i++) dp[i][0] = i;
+        for(int j=0; j<=t.size(); j++) dp[0][j] = j;
+        for(int i=1; i<=s.size(); i++){
+            for(int j=1; j<=t.size(); j++){
+                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1+min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
+            }
+        }
+        return dp[s.size()][t.size()];
     }
     int helper(int i, int j, string& s, string& t, vector<vector<int>>& dp){
         if(i==0) return j;
