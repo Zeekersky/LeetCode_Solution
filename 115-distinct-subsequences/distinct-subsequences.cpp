91979621170082978981f6ff53d586e0c3fcc6e7
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-03 01:15:04 +0530

    Time: 31 ms (57.23%) | Memory: 44.4 MB (18.68%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, -1));
-        return helper(s.size(), t.size(), s, t, dp);
+        // vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, -1));
+        // return helper(s.size(), t.size(), s, t, dp);
+        return tabulation(s, t);
+    }
+    int tabulation(string &s, string &t){
+        int MOD = 1e9 + 7;
+        vector<vector<long long>> dp(s.size()+1, vector<long long>(t.size()+1, 0));
+        for(int i=0; i<=s.size(); i++) dp[i][0] = 1;
+        for(int i=1; i<=s.size(); i++){
+            for(int j=1; j<=t.size(); j++){
+                if(s[i-1]==t[j-1])
+                    dp[i][j] = (dp[i-1][j-1]+dp[i-1][j])%MOD;
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[s.size()][t.size()];
     }
     int helper(int i, int j, string &s, string &t, vector<vector<int>> &dp){
         if(j==0) return 1;
