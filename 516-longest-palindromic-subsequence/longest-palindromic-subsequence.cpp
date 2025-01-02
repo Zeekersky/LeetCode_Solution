0e4214216a50e9cd01f204d6215365bc58d59038
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-02 16:45:39 +0530

    Time: 91 ms (38.92%) | Memory: 76.1 MB (13.70%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -5,22 +5,22 @@ public:
         string text2 = "";
         for(int i=s.size()-1; i>=0; i--)
             text2+=s[i];
-        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), 0));
-        if(text1[0]==text2[0]) dp[0][0] = 1;
-        for(int i=1; i<text1.size(); i++){
-            dp[i][0] = max((text1[i] == text2[0] ? 1 : 0), dp[i-1][0]);
-        }
-        for(int j=1; j<text2.size(); j++){
-            dp[0][j] = max((text1[0] == text2[j] ? 1 : 0), dp[0][j-1]);
-        }
-        for(int i=1; i<text1.size(); i++){
-            for(int j=1; j<text2.size(); j++){
-                if(text1[i]==text2[j])
+        vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1, 0));
+        // if(text1[0]==text2[0]) dp[0][0] = 1;
+        // for(int i=1; i<text1.size(); i++){
+        //     dp[i][0] = max((text1[i] == text2[0] ? 1 : 0), dp[i-1][0]);
+        // }
+        // for(int j=1; j<text2.size(); j++){
+        //     dp[0][j] = max((text1[0] == text2[j] ? 1 : 0), dp[0][j-1]);
+        // }
+        for(int i=1; i<=text1.size(); i++){
+            for(int j=1; j<=text2.size(); j++){
+                if(text1[i-1]==text2[j-1])
                     dp[i][j] = 1+dp[i-1][j-1];
                 else
                     dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
             }
         }
-        return dp[text1.size()-1][text2.size()-1];
+        return dp[text1.size()][text2.size()];
     }
 };
