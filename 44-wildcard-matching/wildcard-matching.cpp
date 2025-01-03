a1101ff2c5976de7a22b4994d5ac4cd7ef269250
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-03 13:36:33 +0530

    Time: 31 ms (70.10%) | Memory: 14.9 MB (66.42%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,8 +1,30 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
-        return helper(s.size(), p.size(), s, p, dp);
+        // vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
+        // return helper(s.size(), p.size(), s, p, dp);
+        return tabulation(s, p);
+    }
+    bool tabulation(string& s, string& t){
+        vector<vector<bool>> dp(s.size()+1, vector<bool>(t.size()+1, false));
+        int j=1;
+        while(j<=t.size() && t[j-1]=='*'){
+            dp[0][j++] = true;
+        }
+        dp[0][0] = true;
+        for(int i=1; i<=s.size(); i++){
+            for(int j=1; j<=t.size(); j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    if(t[j-1]=='?') dp[i][j] = dp[i-1][j-1];
+                    else if(t[j-1]=='*') dp[i][j] = dp[i-1][j] || dp[i-1][j-1] || dp[i][j-1];
+                    else dp[i][j] = false;
+                }
+            }
+        }
+        return dp[s.size()][t.size()];
     }
     bool helper(int i, int j, string& s, string& t, vector<vector<int>>& dp){
         // Base Case
