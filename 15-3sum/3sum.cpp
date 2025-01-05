ebe4e150fcc48a9c512ea49a1d822fd35174b0de
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-05 22:22:27 +0530

    Time: 43 ms (87.77%) | Memory: 28.9 MB (77.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -10,15 +10,16 @@ public:
                 int sum = nums[i] + nums[low] + nums[high];
                 if (sum == 0) {
                     res.push_back({nums[i], nums[low], nums[high]});
-                    while (low < high && nums[low] == nums[low + 1]) low++;
-                    while (low < high && nums[high] == nums[high - 1]) high--;
+                    while (low < high && nums[low] == nums[low + 1]) 
+                        low++;
+                    while (low < high && nums[high] == nums[high - 1]) 
+                        high--;
                     low++;
                     high--;
-                } else if (sum < 0) {
+                } else if (sum < 0)
                     low++;
-                } else {
+                else
                     high--;
-                }
             }
         }
         return res;
