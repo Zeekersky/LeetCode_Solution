e6106643f6ed261af2b584dff15e2fe1b77cfc77
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-09 23:39:26 +0530

    Time: 8 ms (34.70%) | Memory: 12.5 MB (25.84%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(), -1);
-        return helper(0, arr, k, dp);
+        // vector<int> dp(arr.size(), -1);
+        // return helper(0, arr, k, dp);
+        return tabulation(arr, k);
+    }
+    int tabulation(vector<int>& arr, int k){
+        vector<int> dp(arr.size()+1, 0);
+        for(int i=arr.size()-1; i>=0; i--){
+            int maxi = 0, maxEle = arr[i];
+            for(int l=i; l< min(i+k, (int) arr.size()); l++){
+                if(maxEle < arr[l]) maxEle = arr[l];
+                int steps = maxEle*(l-i+1) + helper(l+1, arr, k, dp);
+                maxi = max(maxi, steps);
+            }
+            dp[i] = maxi;
+        }
+        return dp[0];
     }
     int helper(int start, vector<int>& arr, int k, vector<int>& dp){
         // Base Case
