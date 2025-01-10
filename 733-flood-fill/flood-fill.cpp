a54446fb8db8c7ad6a0034cef67f380044824331
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-11 01:53:25 +0530

    Time: 8 ms (2.96%) | Memory: 18.5 MB (14.11%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        // BFS Concept
+        queue<pair<int, int>> q;
+        vector<vector<bool>> vis(image.size(), vector<bool>(image[0].size(), 0));
+        q.push({sr, sc});
+        while(!q.empty()){
+            for(int n = q.size(); n>0; n--){
+                pair<int,int> node = q.front();
+                q.pop();
+                int i=node.first;
+                int j=node.second;
+                int oldVal = image[i][j];
+                image[i][j] = color;
+                if(i>0 && !vis[i-1][j]) {
+                    if(image[i-1][j] == oldVal){
+                        q.push({i-1, j});
+                    }
+                    vis[i-1][j] = 1;
+                }
+                if(j>0 && !vis[i][j-1]) {
+                    if(image[i][j-1] == oldVal){
+                        q.push({i, j-1});
+                    }
+                    vis[i][j-1] = 1;
+                }
+                if(i<image.size()-1 && !vis[i+1][j]) {
+                    if(image[i+1][j] == oldVal){
+                        q.push({i+1, j});
+                    }
+                    vis[i+1][j] = 1;
+                }
+                if(j<image[i].size()-1 && !vis[i][j+1]) {
+                    if(image[i][j+1] == oldVal){
+                        q.push({i, j+1});
+                    }
+                    vis[i][j+1] = 1;
+                }
+            }
+        }
+        return image;
+    }
+};
