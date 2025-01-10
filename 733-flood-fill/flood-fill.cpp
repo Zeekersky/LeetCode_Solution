a52c9dcb2bcff045092f8b3da9923082df36a101
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-11 01:55:10 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (14.11%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -6,37 +6,35 @@ public:
         vector<vector<bool>> vis(image.size(), vector<bool>(image[0].size(), 0));
         q.push({sr, sc});
         while(!q.empty()){
-            for(int n = q.size(); n>0; n--){
-                pair<int,int> node = q.front();
-                q.pop();
-                int i=node.first;
-                int j=node.second;
-                int oldVal = image[i][j];
-                image[i][j] = color;
-                if(i>0 && !vis[i-1][j]) {
-                    if(image[i-1][j] == oldVal){
-                        q.push({i-1, j});
-                    }
-                    vis[i-1][j] = 1;
+            pair<int,int> node = q.front();
+            q.pop();
+            int i=node.first;
+            int j=node.second;
+            int oldVal = image[i][j];
+            image[i][j] = color;
+            if(i>0 && !vis[i-1][j]) {
+                if(image[i-1][j] == oldVal){
+                    q.push({i-1, j});
                 }
-                if(j>0 && !vis[i][j-1]) {
-                    if(image[i][j-1] == oldVal){
-                        q.push({i, j-1});
-                    }
-                    vis[i][j-1] = 1;
+                vis[i-1][j] = 1;
+            }
+            if(j>0 && !vis[i][j-1]) {
+                if(image[i][j-1] == oldVal){
+                    q.push({i, j-1});
                 }
-                if(i<image.size()-1 && !vis[i+1][j]) {
-                    if(image[i+1][j] == oldVal){
-                        q.push({i+1, j});
-                    }
-                    vis[i+1][j] = 1;
+                vis[i][j-1] = 1;
+            }
+            if(i<image.size()-1 && !vis[i+1][j]) {
+                if(image[i+1][j] == oldVal){
+                    q.push({i+1, j});
                 }
-                if(j<image[i].size()-1 && !vis[i][j+1]) {
-                    if(image[i][j+1] == oldVal){
-                        q.push({i, j+1});
-                    }
-                    vis[i][j+1] = 1;
+                vis[i+1][j] = 1;
+            }
+            if(j<image[i].size()-1 && !vis[i][j+1]) {
+                if(image[i][j+1] == oldVal){
+                    q.push({i, j+1});
                 }
+                vis[i][j+1] = 1;
             }
         }
         return image;
