a794497f5da9c51dd155cc4821febd4d1f81879c
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-11 18:50:08 +0530

    Time: 49 ms (67.45%) | Memory: 18.8 MB (29.33%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size(), m=matrix[0].size();
+        // start from the top right corner
         int i=0, j=m-1;
         while(i>=0 && i<n && j>=0 && j<m){
-            // cout << matrix[i][j] << " ";
             if(matrix[i][j] == target) return true;
             else if(matrix[i][j]<target) i++;
             else j--;
