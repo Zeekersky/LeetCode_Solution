685a525a8a4b921ef52fe8c67e767dff6ee4cf82
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-15 23:01:45 +0530

    Time: 3 ms (95.81%) | Memory: 51.9 MB (85.39%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -5,18 +5,18 @@ public:
         vector<int> vis(V, 0);
         vector<int> ans;
         for(int i=0; i<V; i++){
-            if(!vis[i]) dfs(i, -1, adj, vis, ans);
+            if(!vis[i]) dfs(i, adj, vis, ans);
         }
         sort(ans.begin(), ans.end());
         return ans;
     }
-    bool dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ans){
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& ans){
         vis[node] = 1;
         for(int i=0; i<adj[node].size(); i++){
             if(vis[adj[node][i]] == 1){
                 return false; // Cycle detected
             }
-            else if(!vis[adj[node][i]] && !dfs(adj[node][i], node, adj, vis, ans)) 
+            else if(!vis[adj[node][i]] && !dfs(adj[node][i], adj, vis, ans)) 
                 return false; // Any path returns false
         }
         vis[node] = 2;
