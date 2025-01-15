997aa13b8e45fa23ff1922642114a1f052eedb47
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-15 11:16:09 +0530

    Time: 8 ms (41.87%) | Memory: 19.5 MB (59.61%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> dfs;
+        vector<int> vis(numCourses, 0);
+        bool cycle;
+        vector<vector<int>> adj(numCourses);
+        for (auto& it : prerequisites) {
+            adj[it[0]].push_back(it[1]);
+        }
+        for(int i=numCourses-1; i>=0; i--){
+            if(!vis[i])
+                dfsG(i, adj, dfs, vis, cycle);
+            if(cycle) return false;
+        }
+        return true;
+    }
+
+    void dfsG(int node, vector<vector<int>>& adj, vector<int>& dfs, vector<int>& vis, bool& cycle) {
+        if (cycle) return;
+
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            if (vis[it] == 0) {
+                dfsG(it, adj, dfs, vis, cycle);
+            } else if (vis[it] == 1) {
+                cycle = true;
+                return;
+            }
+        }
+        vis[node] = 2;
+        dfs.push_back(node);
+    }
+};
