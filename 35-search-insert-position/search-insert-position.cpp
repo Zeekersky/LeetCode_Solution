e2f6ce7b7e2d96d02fee5b4645b4c5191fb4f160
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-20 19:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (36.31%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        // return binarySearch(nums, target, 0, nums.size()-1);
-        int l=0, h=nums.size()-1;
-        while(l<=h){
-            int mid = (l+h)/2;
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]>target)
-                h = mid-1;
-            else l = mid+1;
-        }
-        return l;
+        return binarySearch(nums, target, 0, nums.size()-1);
     }
     int binarySearch(vector<int>& nums, int tar, int l, int h){
         if(l<=h){
