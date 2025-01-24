26e349adcea5a3851e59f6cc4aed68117adf0932
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-24 15:38:04 +0530

    Time: 7 ms (75.81%) | Memory: 22.8 MB (94.53%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    long long findh(vector<int>&piles,int mid){
-        int n = piles.size();
-        long long ans1 = 0;
-        for(int i=0; i<n; i++){
-            ans1 += (piles[i]/mid) + (piles[i]%mid>0?1:0);
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low=1, high=0;
+        for(auto it:piles) high=max(high, it);
+        int ans = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(checkPossible(mid, piles, h)){
+                ans = mid;
+                high = mid-1;
+            } else low = mid+1;
         }
-        return ans1;
+        return ans;
     }
-    int minEatingSpeed(vector<int>& piles, int hr) {
-        int n = piles.size();
-        int l = 1;
-        int h = *max_element(piles.begin(),piles.end());
-        long long ans =0;
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            ans=findh(piles,mid);
-            if(ans<=hr) {
-                h = mid-1;
-            }
-            else{
-                l = mid+1;
-            }
+    bool checkPossible(int mid, vector<int>& piles, int h){
+        int ans = 0;
+        for(auto it:piles){
+            ans += (it/mid) + (it%mid>0?1:0);
+            if(ans>h) return 0;
         }
-        return l;
+        return 1;
     }
 };
