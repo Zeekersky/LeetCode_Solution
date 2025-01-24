ce7bd048d516c38b1da163651ee3ae6fa069a610
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-24 23:09:24 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (97.58%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -5,12 +5,9 @@ public:
         if(k<arr[0]) return k;
         while(l<=h){
             int mid = (l+h)/2;
-            if(arr[mid]-mid-1 < k){
+            if(arr[mid]-mid-1 < k)
                 l = mid+1;
-            }
-            else {
-                h = mid-1;
-            }
+            else h = mid-1;
         }
         return h+k+1;
     }
