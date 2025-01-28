de749c8b4c67dd29a449a85cb6adc19234246842
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-01-28 11:01:25 +0530

    Time: 3 ms (34.02%) | Memory: 95 MB (93.87%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        if (m > n)
-        {
-            swap(nums1, nums2);
-            swap(m, n);
+    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
+        // code here
+        int n1 = a.size(), n2 = b.size();
+        if(n1>n2) return findMedianSortedArrays(b, a);
+        int left = (n1+n2+1)/2;;
+        int l=0, h=n1;
+        while(l<=h){
+            int mid1 = (l+h)/2;
+            int mid2 = left-mid1;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            if(mid1<n1) r1 = a[mid1];
+            if(mid2<n2) r2 = b[mid2];
+            if(mid1-1>=0) l1 = a[mid1-1];
+            if(mid2-1>=0) l2 = b[mid2-1];
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2 == 0) return ((double)((max(l1, l2)+min(r1, r2))/2.0));
+                return max(l1, l2);
+            } 
+            if(l1>r2) h=mid1-1;
+            else l=mid1+1;
         }
-        int low = 0, high = m;
-        while (low <= high)
-        {
-            int mid1 = (low + high) / 2;
-            int mid2 = (m + n + 1) / 2 - mid1;
-            int r1 = INT_MAX, r2 = INT_MAX, l1 = INT_MIN, l2 = INT_MIN;
-
-            if (mid1 < m)
-                r1 = nums1[mid1];
-            if (mid2 < n)
-                r2 = nums2[mid2];
-
-            if (mid1 > 0)
-                l1 = nums1[mid1 - 1];
-            if (mid2 > 0)
-                l2 = nums2[mid2 - 1];
-
-            if (l1 > r2)
-                high = mid1 - 1;
-            else if (l2 > r1)
-                low = mid1 + 1;
-            else
-            {
-                int maxLeft = max(l1, l2);
-                int minRight = min(r1, r2);
-                if ((m + n) % 2 == 0)
-                    return (double)(maxLeft + minRight) / 2.0;
-                else
-                    return maxLeft;
-            }
-        }
-        return 0.0;
+        return 0;
     }
 };
