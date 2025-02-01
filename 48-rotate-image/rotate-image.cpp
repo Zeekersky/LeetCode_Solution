cb7ba0e6fb98147f6645b9c9a0b48781e8b023dc
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-02-01 18:48:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (8.51%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,17 +4,12 @@ public:
         for(int i=0; i<matrix.size(); i++)
         {
             for(int j=0; j<matrix[0].size(); j++)
-            {
-                if(i<j)
-                    swap(matrix[i][j], matrix[j][i]);
-            }
+                if(i<j) swap(matrix[i][j], matrix[j][i]);
         }
         for(int i=0; i<matrix.size(); i++)
         {
             for(int j=0; j<((matrix[i].size())/2); j++)
-            {
                 swap(matrix[i][j], matrix[i][(matrix[0].size())-j-1]);
-            }
         }
     }
 };
