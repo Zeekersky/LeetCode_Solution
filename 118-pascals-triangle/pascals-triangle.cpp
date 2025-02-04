be780a74a7a8f4cf92ba73520b88a808d0c53446
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-02-04 16:38:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (64.10%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=1; i<=numRows; i++){
+            int col = i;
+            vector<int> temp;
+            while(col>0){
+                // cout << i-1 << " " << i-col << " " << endl;
+                temp.push_back(findNcR(i-1, i-col));
+                col--;
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+    long long  findNcR(int n, int r){
+        long long res = 1;
+        for(int i=0; i<r; i++){
+            res = res*(n-i);
+            res = res/(i+1);
+        }
+        return res;
+    }
+};
