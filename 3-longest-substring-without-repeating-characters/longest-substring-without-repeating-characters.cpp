2562f0c7ed131743a7a92e15ad8b90d3123041db
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-02-06 03:25:50 +0530

    Time: 383 ms (7.54%) | Memory: 11.8 MB (68.15%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,17 +4,10 @@ public:
         vector<int> arr(256, -1);
         int l = 0, r = 0, maxi = 0;
         while(r<s.length()) {
-            if(arr[s[r]] == -1){
-                arr[s[r]] = r;
+            if(arr[s[r]] != -1){
+                l = max(arr[s[r]]+1, l);
             }
-            else {
-                while(l<arr[s[r]] + 1){
-                    if(arr[s[l]] == l)
-                        arr[s[l]] = -1;
-                    l++;
-                }
-                arr[s[r]] = r;
-            } 
+            arr[s[r]] = r;
             cout << "l: " << l << " r: " << r << endl;
             maxi = max(maxi, r-l+1);
             r++;
