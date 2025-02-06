a78ba30ccef92d4889b90f841aef9a19447b0285
Akash Pal <55991971+Zeekersky@users.noreply.github.com>
2025-02-06 11:21:05 +0530

    Time: 58 ms (47.98%) | Memory: 50.8 MB (28.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,8 +5,8 @@ public:
         int sum=0;
         int cnt=0;
         mpp[0] = 1;
-        for(int i=0; i<nums.size(); i++){
-            sum+=nums[i];
+        for(auto it:nums){
+            sum+=it;
             cnt+=mpp[sum-k];
             mpp[sum]++;
         }
